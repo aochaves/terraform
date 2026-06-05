85c01d63246bb8637c6e4abe251275fd0dcb523d
EC2 Default User <ec2-user@ip-172-31-13-210.ec2.internal>
2026-06-05 21:34:42 +0000

    Trabalho terraform V 1.1

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+
+output "alb_dns_name" {
+  description = "DNS do Application Load Balancer"
+  value       = aws_lb.app_alb.dns_name
 }
